decbcaf6bc9ab95e6a0d4ac931927bd56903a86e
saqibbedar <chandio.saqib.m@gmail.com>
2026-07-08 18:40:04 +0500

    feat: add range programs and few facts

---
+++ python_2026/0x_iterators/03_range/00_output_questions/01.py
@@ -0,0 +1,7 @@
+r: range = range(1, 6)
+print(r)
+
+"""
+output: range(1, 6) :- Why? 
+Because range stores only three integers value i.e., range(start : int = 0, stop : int, step : int = 1) not all values.
+"""
---
+++ python_2026/0x_iterators/03_range/03_list&range.py
@@ -0,0 +1,8 @@
+fruits: str = ["Apple", "Banana", "Carrot"]
+
+for i in range(len(fruits)):
+    print(f"fruit name is [{fruits[i]}]")
+ 
+# fruit name is [Apple]
+# fruit name is [Banana]
+# fruit name is [Carrot]
