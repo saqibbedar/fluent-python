68c1b3fda83b74a2d84968e03d04de3e84cef546
saqibbedar <chandio.saqib.m@gmail.com>
2026-08-17 00:04:48 +0500

    feat(numpy): add new numpy notebook files and added snippets in previous notebooks

---
+++ core_libs/numpy/quicks/02_numpy_constants.py
@@ -0,0 +1,21 @@
+# For more check NumPy official documentation on constants
+# https://numpy.org/doc/stable/reference/constants.html 
+
+import numpy as np
+
+# PI
+print(np.pi)                        # 3.141592653589793
+
+# Euler's number
+print(np.e)                         # 2.718281828459045
+
+print(np.euler_gamma)               # 0.5772156649015329
+
+# IEEE 754 floating point representation of (positive) infinity
+print(np.inf)                       # inf
+
+# IEEE 754 floating point representation of Not a Number (NaN)
+print(np.nan)                       # nan
+
+# A convenient alias for None, useful for indexing arrays
+print(np.newaxis)                   # None
