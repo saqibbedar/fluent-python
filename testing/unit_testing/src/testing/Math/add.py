729e7115c5ef6b2901c1a2c9d787af387421ec9f
Saqib Bedar <124094939+saqibbedar@users.noreply.github.com>
2026-09-25 23:33:40 +0500

    refactor: modify testing dir & resolve os changes [windows->linux]

--- testing/unit_testing/src/testing/Math/add.py
+++
@@ -1,5 +0,0 @@
-# input integer a, integer b
-# return integer a + b
-def add(a: int, b: int) -> int:
-    """Return integer sum a + b"""
-    return a + b
--- testing/unit_testing/src/testing/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Welcome to testing world!!")
-
-
-if __name__ == "__main__":
-    main()
